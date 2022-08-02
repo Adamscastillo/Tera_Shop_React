620e13319370b033d9c802c1981be46189927c91
Maycon Godoy <mykooon@hotmail.com>
2022-08-02 20:52:40 -0300

    page cadastro

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "../Cadastro/cadastro.css";
+
 
 function Cadastro() {
   return (
-    <div>
-      <div>cadastro</div>
+    <div className="main-cadastro">
+      <Form className="form">
+        <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+          <Form.Label>Digite seu E-mail</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted bg-dark">
+            Digite seu melhor E-mail  
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3 form-group">
+          <Form.Label>CPF</Form.Label>
+          <Form.Control type="text" placeholder="000-000-000-00" pattern="(\d{3}\.?\d{3}\.?\d{3}-?\d{2})|(\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2})"/>         
+        </Form.Group>
+
+        <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+          <Form.Label>Telefone</Form.Label>
+          <Form.Control type="tel" placeholder="(00)00000-0000" />          
+        </Form.Group>
+
+        <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+          <Form.Label>CEP</Form.Label>
+          <Form.Control type="cep" placeholder="00000-000" />          
+        </Form.Group>
+
+        <Form.Group className="mb-3 form-group" controlId="formBasicPassword">
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" placeholder="Senha" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Enviar
+        </Button>
+      </Form>
     </div>
-  )
+  );
 }
 
-export default Cadastro
+export default Cadastro;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import NavBar from '../../components/header/index'
 import CarouselImagens from '../../components/carroussel/index'
 import CardProdutos from '../../components/cardsProducts/index'
 import '../../../src/App.css'
 import ListTenis from '../../components/TenisAPi/TenisApi'
-import Footer from '../../components/footer/Footer'
 import ImagePromo from '../../components/promoção/promo'
 import TenisPromo from '../../components/TenisPromo/TenisPromo'
 
