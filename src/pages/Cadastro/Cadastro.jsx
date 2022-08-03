357f05644e50f0b3c5dad2d7ecc6cbd28150af3b
Maycon Godoy <mykooon@hotmail.com>
2022-08-03 18:23:23 -0300

    meu commit

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -8,6 +8,11 @@ function Cadastro() {
   return (
     <div className="main-cadastro">
       <Form className="form">
+      <Form.Group className="mb-3 form-group"  >
+          <Form.Label>Nome</Form.Label>
+          <Form.Control type="text" placeholder="000-000-000-00" pattern="(\d{3}\.?\d{3}\.?\d{3}-?\d{2})|(\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2})"/>         
+        </Form.Group>
+
         <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
           <Form.Label>Digite seu E-mail</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
@@ -18,20 +23,20 @@ function Cadastro() {
 
         <Form.Group className="mb-3 form-group">
           <Form.Label>CPF</Form.Label>
-          <Form.Control type="text" placeholder="000-000-000-00" pattern="(\d{3}\.?\d{3}\.?\d{3}-?\d{2})|(\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2})"/>         
+          <Form.Control type="cpf" autoComplete="tel" placeholder="000-000-000-00" maxLength={'11'} pattern="(\d{3}\.?\d{3}\.?\d{3}-?\d{2})|(\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2})"/>         
         </Form.Group>
 
-        <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+        <Form.Group className="mb-3 form-group" >
           <Form.Label>Telefone</Form.Label>
           <Form.Control type="tel" placeholder="(00)00000-0000" />          
         </Form.Group>
 
-        <Form.Group className="mb-3 form-group" controlId="formBasicEmail">
+        <Form.Group className="mb-3 form-group" >
           <Form.Label>CEP</Form.Label>
           <Form.Control type="cep" placeholder="00000-000" />          
         </Form.Group>
 
-        <Form.Group className="mb-3 form-group" controlId="formBasicPassword">
+        <Form.Group className="mb-3 form-group">
           <Form.Label>Senha</Form.Label>
           <Form.Control type="password" placeholder="Senha" />
         </Form.Group>
